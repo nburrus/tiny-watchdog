a7ed544980222f744d256c54b335b8d3ec93c0ab
Nicolas Burrus <nb@arcturus.industries>
2021-05-31 21:53:23 +0200

    Automatically restart watchdog when it throws an exception, it happens.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,11 @@
 export PYTHONUNBUFFERED=1
 
 cd /deploy
-./watchdog.py "$WATCHDOG_URL" --image-server-password "$IMAGE_SERVER_PASSWORD" --data-dir /data |& tee /data/watchdog.log &
-./web_server/web_server.py --urlpath "$URL_PATH" --data-dir /data |& tee /data/web_server.log
+
+./web_server/web_server.py --urlpath "$URL_PATH" --data-dir /data |& tee -a /data/web_server.log &
+
+# Keep launching it as it tends to throw exceptions sometimes
+while true; do
+    ./watchdog.py "$WATCHDOG_URL" --image-server-password "$IMAGE_SERVER_PASSWORD" --data-dir /data |& tee -a /data/watchdog.log
+    sleep 1
+done
